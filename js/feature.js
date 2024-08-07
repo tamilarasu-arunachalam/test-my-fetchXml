4936946767a213980664f01dd1a85fb8bcdea586
Tamilarasu Arunachalam <tamilarasuva444@gmail.com>
2024-08-07 16:43:00 +0530

    F1: Adding org from local storage F2: Some Optimization Changes

--- js/feature.js
+++ js/feature.js
@@ -1,9 +1,16 @@
+var testBtn = document.getElementById("testBtn");
+var orgUrl = document.getElementById("orgUrl");
+var entityName = document.getElementById("entityName");
+var fetchXmlQuery = document.getElementById("fetchXmlQuery");
+
+if (localStorage.getItem("orgUrl") !== null)
+    orgUrl.value = localStorage.getItem("orgUrl");
+
 // set plural name
 setInterval(function () {
     var parser, xmlParse;
-    var fetchXmlQuery = document.getElementById("fetchXmlQuery").innerText.replaceAll('\n', '');
     parser = new DOMParser();
-    xmlParse = parser.parseFromString(fetchXmlQuery, "text/xml");
+    xmlParse = parser.parseFromString(fetchXmlQuery.innerText.replaceAll('\n', ''), "text/xml");
     var node = xmlParse.getElementsByTagName("entity")[0];
     var attr = node.attributes[0];
     var entity = attr.value;
@@ -13,22 +20,21 @@ setInterval(function () {
 
 //on click of test fetch xml button
 function testOnClick() {
-    var testBtn = document.getElementById("testBtn");
-    var orgUrl = document.getElementById("orgUrl").value;
-    var entityName = document.getElementById("entityName").value;
-    if (orgUrl === '') {
+    if (orgUrl.value === '') {
         alert("Please enter Organization URL");
     } else {
-        var fetchXmlQuery = document.getElementById("fetchXmlQuery").innerText.replaceAll('\n', '');
-        testBtn.setAttribute("href", orgUrl + "api/data/v9.2/" + entityName + "?fetchXml=" + fetchXmlQuery);
+        testBtn.setAttribute("href", orgUrl.value + "/api/data/v9.2/" + entityName.value + "?fetchXml=" + fetchXmlQuery.innerText.replaceAll('\n', '').replace(/\s{2,}/g, ' '));
         testBtn.setAttribute("target", "_blank");
     }
 }
 
 // get plural name of the entity 
 function getPlural(lastChar, entity) {
-    var entityName = document.getElementById("entityName");
     switch (lastChar) {
+        case 'x':
+        case 's':
+            entityName.value = entity + "es";
+            break;
         case 'y':
             var pref = entity.substring(0, entity.length - 1);
             entityName.value = pref + "ies";
@@ -36,5 +42,27 @@ function getPlural(lastChar, entity) {
         default:
             entityName.value = entity + "s";
             break;
+    }
+}
+
+function addOrgToLocalStorage() {
+    if (orgUrl.value.length === 0)
+        return;
+    if (isValidURL(orgUrl.value)) {
+        var url = new URL(orgUrl.value);
+        orgUrl.value = url.origin;
+        localStorage.setItem("orgUrl", orgUrl.value);
+    } else {
+        alert("Please enter a valid URL.");
+    }
+};
+
+function isValidURL(str) {
+    regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+    if (regexp.test(str)) {
+        return true;
+    }
+    else {
+        return false;
     }
 }
