91989832f6af92085f7b1148a4f130a240667e8c
Tamilarasu Arunachalam <tamilarasuva444@gmail.com>
2025-02-07 14:56:59 +0530

    Update feature.js

--- js/feature.js
+++ js/feature.js
@@ -3,14 +3,31 @@ var orgUrl = document.getElementById("orgUrl");
 var entityName = document.getElementById("entityName");
 var fetchXmlQuery = document.getElementById("fetchXmlQuery");
 
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector("#creditYear").textContent = new Date().getFullYear();
+});
+
+var editor = CodeMirror.fromTextArea(document.getElementById("fetchXmlQuery"), {
+    mode: "application/xml",
+    lineNumbers: true,
+    theme: "default"
+});
+
+document.getElementById("creditYear").innerHTML = new Date.getFullYear();
+
+setInterval(function() {
+    console.log(editor.getValue());
+}, 2000);
+
 if (localStorage.getItem("orgUrl") !== null)
     orgUrl.value = localStorage.getItem("orgUrl");
 
 // set plural name
-setInterval(function () {
+setInterval(function() {
     var parser, xmlParse;
     parser = new DOMParser();
-    xmlParse = parser.parseFromString(fetchXmlQuery.innerText.replaceAll('\n', ''), "text/xml");
+    let onlyXml = editor.getValue();
+    xmlParse = parser.parseFromString(onlyXml, "text/xml");
     var node = xmlParse.getElementsByTagName("entity")[0];
     var attr = node.attributes[0];
     var entity = attr.value;
@@ -18,12 +35,26 @@ setInterval(function () {
     getPlural(lastChar, entity);
 }, 2000);
 
+// onchange of xml
+function xmlOnChange() {
+    var fetchXmlQuery = document.getElementById("fetchXmlQuery");
+    var parser, xmlParse;
+    parser = new DOMParser();
+    let onlyXml = fetchXmlQuery.value.replaceAll('\n', '');
+    xmlParse = parser.parseFromString(onlyXml, "text/xml");
+    var node = xmlParse.getElementsByTagName("entity")[0];
+    var attr = node.attributes[0];
+    var entity = attr.value;
+    var lastChar = entity.charAt(entity.length - 1);
+    getPlural(lastChar, entity);
+}
+
 //on click of test fetch xml button
 function testOnClick() {
     if (orgUrl.value === '') {
         alert("Please enter Organization URL");
     } else {
-        testBtn.setAttribute("href", orgUrl.value + "/api/data/v9.2/" + entityName.value + "?fetchXml=" + fetchXmlQuery.innerText.replaceAll('\n', '').replace(/\s{2,}/g, ' '));
+        testBtn.setAttribute("href", orgUrl.value + "/api/data/v9.2/" + entityName.value + "?fetchXml=" + fetchXmlQuery.value.replaceAll('\n', '').replace(/>\s*/g, '>').replace(/\s*</g, '<'));
         testBtn.setAttribute("target", "_blank");
     }
 }
@@ -61,8 +92,7 @@ function isValidURL(str) {
     regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
     if (regexp.test(str)) {
         return true;
-    }
-    else {
+    } else {
         return false;
     }
 }
